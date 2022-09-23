276e10a22ca5c8ed7f9fb75978185a86d1e71aa6
hiarief <arhistr14@gmail.com>
2022-09-23 11:29:43 +0700

    arief

--- resources/views/legalitas-perusahaan/index.blade.php
+++ resources/views/legalitas-perusahaan/index.blade.php
@@ -1,5 +1,56 @@
 @extends('layouts.app')
 @section('title', 'Legalitas Perusahaan')
 @section('content')
+    <main id="main">
 
+        <!-- ======= Breadcrumbs ======= -->
+        <div class="breadcrumbs d-flex align-items-center"
+            style="background-image: url(assets/assets/img/breadcrumbs-bg.jpg);">
+            <div class="container position-relative d-flex flex-column align-items-center" data-aos="fade">
+
+                <h2>Legalitas Perusahaan</h2>
+                <ol>
+                    <li><a href="{{ route('front.index') }}">Home</a></li>
+                    <li>Legalitas Perusahaan</li>
+                </ol>
+
+            </div>
+        </div>
+
+
+        <section id="projects" class="projects">
+            <div class="container" data-aos="fade-up">
+
+                <div class="portfolio-isotope" data-portfolio-filter="*" data-portfolio-layout="masonry"
+                    data-portfolio-sort="original-order">
+
+                    <div class="row gy-4 portfolio-container" data-aos="fade-up" data-aos-delay="200">
+
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/legalitas/l1.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/legalitas/l1.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/legalitas/l2.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/legalitas/l2.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </section>
+
+    </main>
 @endsection
--- resources/views/service/index.blade.php
+++ resources/views/service/index.blade.php
@@ -1,5 +1,182 @@
 @extends('layouts.app')
 @section('title', 'Service')
 @section('content')
+    <main id="main">
 
+        <!-- ======= Breadcrumbs ======= -->
+        <div class="breadcrumbs d-flex align-items-center"
+            style="background-image: url(assets/assets/img/breadcrumbs-bg.jpg);">
+            <div class="container position-relative d-flex flex-column align-items-center" data-aos="fade">
+
+                <h2>Service</h2>
+                <ol>
+                    <li><a href="{{ route('front.index') }}">Home</a></li>
+                    <li>Service</li>
+                </ol>
+
+            </div>
+        </div>
+
+
+        <section id="projects" class="projects">
+            <div class="container" data-aos="fade-up">
+
+                <div class="portfolio-isotope" data-portfolio-filter="*" data-portfolio-layout="masonry"
+                    data-portfolio-sort="original-order">
+
+                    <div class="row gy-4 portfolio-container" data-aos="fade-up" data-aos-delay="200">
+
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/service/hal1.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/service/hal1.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/service/hal2.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/service/hal2.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/service/hal3.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/service/hal3.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/service/hal4.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/service/hal4.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/service/hal5.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/service/hal5.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/service/hal6.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/service/hal6.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/service/hal7.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/service/hal7.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/service/hal8.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/service/hal8.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/service/hal9.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/service/hal9.jpg"
+                                        data-gallery="portfolio-gallery-remodeling" class="glightbox preview-link"><i
+                                            class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/service/hal10.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/service/hal10.jpg"
+                                        data-gallery="portfolio-gallery-remodeling" class="glightbox preview-link"><i
+                                            class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/service/hal11.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/service/hal11.jpg"
+                                        data-gallery="portfolio-gallery-remodeling" class="glightbox preview-link"><i
+                                            class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/service/hal12.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/service/hal12.jpg"
+                                        data-gallery="portfolio-gallery-remodeling" class="glightbox preview-link"><i
+                                            class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/service/hal13.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/service/hal13.jpg"
+                                        data-gallery="portfolio-gallery-remodeling" class="glightbox preview-link"><i
+                                            class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/service/hal14.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/service/hal14.jpg"
+                                        data-gallery="portfolio-gallery-remodeling" class="glightbox preview-link"><i
+                                            class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/service/hal15.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/service/hal15.jpg"
+                                        data-gallery="portfolio-gallery-remodeling" class="glightbox preview-link"><i
+                                            class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+                    </div>
+
+                </div>
+
+            </div>
+        </section>
+
+    </main>
 @endsection
