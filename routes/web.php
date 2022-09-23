8bed16f877c080396e740bd841d07907dbbe8311
hiarief <arhistr14@gmail.com>
2022-09-23 10:19:12 +0700

    arief

--- routes/web.php
+++ routes/web.php
@@ -8,20 +8,7 @@ use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\LegalitasController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-// Route::get('/', function () {
-//     return view('front\index');
-// });
 
 Route::get('/', [FrontController::class, 'index'])->name('front.index');
 Route::get('/about', [AboutController::class, 'index'])->name('about.index');
