6b39581a9dbd01090d9d8d187cda483d46ec54dc
hiarief <arhistr14@gmail.com>
2022-09-23 22:04:30 +0700

    arief

--- resources/views/legalitas-perusahaan/index.blade.php
+++ resources/views/legalitas-perusahaan/index.blade.php
@@ -44,6 +44,267 @@
                                 </div>
                             </div>
                         </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/notaris/01.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/notaris/01.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/notaris/02.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/notaris/02.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/notaris/03.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/notaris/03.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/notaris/04.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/notaris/04.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/notaris/05.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/notaris/05.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/notaris/06.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/notaris/06.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/notaris/07.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/notaris/07.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/notaris/08.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/notaris/08.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/notaris/09.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/notaris/09.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/notaris/10.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/notaris/10.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/notaris/11.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/notaris/11.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/notaris/12.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/notaris/12.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/notaris/13.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/notaris/13.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/notaris/14.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/notaris/14.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/notaris/15.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/notaris/15.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/notaris/16.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/notaris/16.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/notaris/17.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/notaris/17.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/notaris/18.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/notaris/18.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/notaris/19.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/notaris/19.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/notaris/20.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/notaris/20.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/notaris/21.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/notaris/21.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/notaris/22.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/notaris/22.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/notaris/23.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/notaris/23.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/notaris/24.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/notaris/24.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/notaris/25.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/notaris/25.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/notaris/26.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/notaris/26.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/notaris/27.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/notaris/27.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/notaris/28.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/notaris/28.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 portfolio-item">
+                            <div class="portfolio-content h-100">
+                                <img src="assets/assets/img/notaris/29.jpg" class="img-fluid" alt="">
+                                <div class="portfolio-info">
+                                    <a href="assets/assets/img/notaris/29.jpg" data-gallery="portfolio-gallery-remodeling"
+                                        class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
 
                     </div>
 
